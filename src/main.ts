b1a40a5823afe9f2b78612b80c57417b0d840e9d
Pal Bijewar <pal.bijewar@gmail>
2025-04-04 19:00:05 +0530

    fixes:cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://yourfrontenddomain.com'],
+    origin: [
+      'http://localhost:3000',                 // local dev
+      'https://investingarrowbot.com',         // your frontend
+      'https://api.investingarrowbot.com'      // your new backend domain
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   });  
