97befbaf46151e517beaf9a8c21abfd8c06238e4
Pal Bijewar <pal.bijewar@gmail>
2025-04-04 18:55:59 +0530

    fixes:cors

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 // main.ts
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://yourfrontenddomain.com'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  });  
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
