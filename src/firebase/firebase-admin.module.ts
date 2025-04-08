9e7124842d46e1089d9d3ea27888aaa96297c329
Pal Bijewar <pal.bijewar@gmail>
2025-04-08 12:05:28 +0530

    added: ew http

--- src/firebase/firebase-admin.module.ts
+++ src/firebase/firebase-admin.module.ts
@@ -1,18 +1,18 @@
-import { Module, Global } from '@nestjs/common';
-import * as admin from 'firebase-admin';
+// import { Module, Global } from '@nestjs/common';
+// import * as admin from 'firebase-admin';
 
-import * as serviceAccount from './serviceAccountKey.json'; 
+// import * as serviceAccount from './serviceAccountKey.json'; 
 
-@Global()
-@Module({
-  providers: [
-    {
-      provide: 'FIREBASE_ADMIN',
-      useValue: admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-      }),
-    },
-  ],
-  exports: ['FIREBASE_ADMIN'],
-})
-export class FirebaseAdminModule {}
+// @Global()
+// @Module({
+//   providers: [
+//     {
+//       provide: 'FIREBASE_ADMIN',
+//       useValue: admin.initializeApp({
+//         credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+//       }),
+//     },
+//   ],
+//   exports: ['FIREBASE_ADMIN'],
+// })
+// export class FirebaseAdminModule {}
