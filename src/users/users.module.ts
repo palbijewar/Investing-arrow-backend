79aca5d459a761e63bf9b3dc552b5946fdf57066
palbijewar <pal.bijewar@gmail.com>
2025-05-26 13:58:00 +0530

    fixes: getSponsors

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,12 +4,14 @@ import { User, UserSchema } from './user.schema';
 import { PaymentOption, PaymentOptionSchema } from 'src/payment-options/payment-options.schema';
 import { UsersService } from './users.service';
 import { MailService } from './mail.service';
+import { GasWallet, GasWalletSchema } from 'src/gaswallet/GasWallet.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: User.name, schema: UserSchema },
       { name: PaymentOption.name, schema: PaymentOptionSchema },
+      { name: GasWallet.name, schema: GasWalletSchema },
     ]),
   ],
   providers: [UsersService, MailService],
